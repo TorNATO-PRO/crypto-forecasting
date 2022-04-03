59ca16630e83fc08ea66b8c1446362a9791922f7
Nathan Waltz <nathan.waltz@wsu.edu>
2022-04-02 22:00:38 -0700

    Now made it display shares and price for easier interpretibility

--- main.py
+++ main.py
@@ -54,46 +54,44 @@ torch.manual_seed(seed)
 start_date = '2018-01-01'
 end_date = '2021-01-01'
 start_pred_date = '2021-01-01'
-end_pred_date = '2022-04-01'
+end_pred_date = '2022-01-01'
 
 
 data_loader = DataLoader()
 crypto_dataset = CryptoDataset('BITCOIN', 'BTC-USD.csv')
 dataset = data_loader.load_data(crypto_dataset)
 
-
-header = '#########################################'
 oracle_msg = 'Learning using Oracle'
-oracle_pad_len = (len(header) - len(oracle_msg) - 2) // 2
+oracle_pad_len = (len(oracle_msg)) // 2 - 1
 custom_msg = 'Learning using Custom Model'
-custom_pad_len = (len(header) - len(custom_msg) - 2) // 2
+custom_pad_len = (len(custom_msg)) // 2 - 1
 summary_msg = 'Learning using Summary'
-summary_pad_len = (len(header) - len(summary_msg) - 2) // 2
+summary_pad_len = (len(summary_msg)) // 2 - 1
 
-
-print("#########################################")
+print("#" * len(oracle_msg) * 2)
 print('#' + oracle_pad_len * ' ' + oracle_msg +  oracle_pad_len * ' ' + '#')
-print("#########################################")
+print("#" * len(oracle_msg) * 2)
+
 _, oracle_model = oracle.train_model(dataset, start_date, end_date, params_oracle)
 ora_preds, buy_hold_preds, ora_trades = oracle.evaluate(dataset, start_pred_date, end_pred_date, params_oracle, oracle_model)
 
-d = [[f'Day {day}', action] for day, action in enumerate(ora_trades)]
-df = pd.DataFrame(d, columns = ['Day of Trading', 'Shares Bought'])
+d = [[f'Day {day}', action, returns] for day, (action, returns) in enumerate(zip(ora_trades, ora_preds))]
+df = pd.DataFrame(d, columns = ['Day of Trading', 'Shares Owned', 'Model Profits (USD)'])
 print(tabulate(df, headers='keys', tablefmt='psql', showindex=False))
 
-print("#########################################")
+print("#" * len(custom_msg) * 2)
 print('#' + custom_pad_len * ' ' + custom_msg +  custom_pad_len * ' ' + '#')
-print("#########################################")
+print("#" * len(custom_msg) * 2)
 _, custom_model = custom.train_model(dataset, start_date, end_date, params_custom, ['Open'])
 cus_preds, cus_trades = custom.evaluate(dataset, start_pred_date, end_pred_date, params_custom, ['Open'], custom_model)
 
-d = [[f'Day {day}', action] for day, action in enumerate(cus_trades)]
-df = pd.DataFrame(d, columns = ['Day of Trading', 'Shares Bought'])
+d = [[f'Day {day}', action, returns] for day, (action, returns) in enumerate(zip(cus_trades, cus_preds))]
+df = pd.DataFrame(d, columns = ['Day of Trading', 'Shares Owned', 'Model Profits (USD)'])
 print(tabulate(df, headers='keys', tablefmt='psql', showindex=False))
 
-print("#########################################")
+print("#" * len(summary_msg) * 2)
 print('#' + summary_pad_len * ' ' + summary_msg +  summary_pad_len * ' ' + '#')
-print("#########################################")
+print("#" * len(summary_msg) * 2)
 d = [ ["Buy and Hold", round(buy_hold_preds[-1], 4), np.mean(buy_hold_preds)],
      ["Oracle", round(ora_preds[-1], 4), np.mean(ora_preds)],
      ["Custom", round(cus_preds[-1], 4), np.mean(cus_preds)]]
@@ -108,7 +106,7 @@ input()
 assert(len(ora_preds) == len(buy_hold_preds) == len(cus_preds))
 
 # plotting logic
-plt.title(f'Trading evaluation from {start_pred_date} to {end_pred_date} on {str(crypto_dataset)}')
+plt.title(f'Trading evaluation from {(pd.Timestamp(end_pred_date) - pd.Timedelta(days=len(ora_preds))).strftime("%m/%d/%y")} to {pd.Timestamp(end_pred_date).strftime("%m/%d/%y")} on {str(crypto_dataset)}')
 plt.plot(ora_preds, label='Oracle Returns')
 plt.plot(buy_hold_preds, label='Buy and Hold Returns')
 plt.plot(cus_preds, label='Model Returns')
